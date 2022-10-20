a19b6d53b205b1cac390459aba94e44d2478bb2e
Max Lamparth <72386596+maxlampe@users.noreply.github.com>
2022-10-20 17:29:19 +0200

    feat: Added backscattering in simulation loop.

--- src/main.jl
+++ src/main.jl
@@ -5,19 +5,22 @@ include("plotting.jl")
 include("tools.jl")
 
 # Bi trigger rate ca. 4500 Hz; Bg trigger rate ca 500 Hz
-bi_rate = 4.5e3
+bi_rate = 4.6e1
 gain = 1. # [1/keV] or 30.89 [ch/keV]
 srcs = Dict(
-    "bi1" => Source(bi_rate * 0.7, gain * 450., gain * 35.),
-    "bi1_1" => Source(bi_rate * 0.25, gain * 520., gain * 35.),
-    "bi1_2" => Source(bi_rate * 0.1, gain * 570., gain * 30.),
-    "bi2" => Source(bi_rate * 4., gain * 1000., gain * 60.),
-    "bi0" => Source(bi_rate * 1.2, gain * 70., gain * 15.),
+    "bi1" => Source(bi_rate * 0.7 / 7.0, gain * 450., gain * 35.),
+    "bi1_1" => Source(bi_rate * 0.25 / 7.0, gain * 520., gain * 35.),
+    "bi1_2" => Source(bi_rate * 0.1 / 7.0, gain * 570., gain * 30.),
+    "bi2" => Source(bi_rate * 4. / 7.0, gain * 1000., gain * 60.),
+    "bi0/bg" => Source(bi_rate * 1.95 / 7.0, gain * 70., gain * 19.),
+)
+srcs = Dict(
+    "mono" => Source(bi_rate, gain * 400., gain * 0.),
 )
 
-
-res = run_sim(srcs; n_det=2, meas_time=10.)
+res = run_sim(srcs; n_det=2, meas_time=5., with_bs=false)
 plot_e_hist(res, gain)
+plot_diff_both_trig(res)
 plot_e_acc_hist(res, gain)
 plot_e_both_trig(res, gain)
 plot2D_e_both_trig(res, gain)
--- src/simulation.jl
+++ src/simulation.jl
@@ -1,5 +1,8 @@
 
+
+using Printf
 include("structs.jl")
+include("tools.jl")
 
 
 # ToDo: currently only works for dtime * SIM_STEP << 1
@@ -14,12 +17,25 @@ function create_energy(src_dict::Dict, dtime::Float64)::Float64
 end
 
 
-function run_sim(src_dict::Dict; meas_time::Float64 = 1e0, sim_step::Float64 = 1e-8, n_det::Int64 = 1)
-    det_main = Detector()
+function run_sim(
+    src_dict::Dict;
+    meas_time::Float64 = 1e0,
+    sim_step::Float64 = 5e-9,
+    n_det::Int64 = 1,
+    with_bs::Bool = false,
+    t_meas::Float64 = 2e-7,
+)
+    det_main = Detector(t_meas=t_meas)
+
+    # calculate theoretical, pure rate from source
+    theo_rate = 0.
+    for src in src_dict
+        theo_rate += src[2].rate
+    end
 
     n_time_steps = meas_time/sim_step
     # Should be large enough buffer
-    data = Vector{Event}(undef, convert(Int, n_time_steps * 0.01))
+    data = Vector{Event}(undef, convert(Int, theo_rate * meas_time * n_det * 2.0)) # n_time_steps * 0.01
     data_ind = 1
     curr_ev = Event()
     t_last_ev = 0.
@@ -46,25 +62,48 @@ function run_sim(src_dict::Dict; meas_time::Float64 = 1e0, sim_step::Float64 = 1
             # create energy from all sources at this time step
             e_now = create_energy(src_dict, sim_step)
 
-            # detect energy
-            if det_main.is_measuring == true
-                if e_now >= 0.1
-                    if curr_ev.t_trig[i] < 0
-                        curr_ev.t_trig[i] = t_now
-                    end
-                    curr_ev.e_det += e_now
-                    curr_ev.n_sum += 1
-                    curr_ev.e_ind[i] += e_now
+            e_split = [0.0, 0.0]
+            e_split[i] = e_now
+            t_tof_now = 0.0
+
+            # do backscattering process
+            if e_now > 0.1 && with_bs
+                p = prob_bs(e_now)
+                if p >= rand()
+                    frac_bs = e_bs_frac(e_now)
+                    e_split[i] = e_now * frac_bs
+                    e_split[(i % 2) + 1] = e_now * (1.0 - frac_bs)
+                    t_tof_now = t_tof(e_now)   
                 end
-            else
-                if e_now >= det_main.thresh
-                    # Todo: This adds trigger bias towards first detector. Should not be a problem as dt = 1e-8
-                    timing = [-1., -1.]
-                    timing[i] = t_now
-                    e_ind = [0., 0.]
-                    e_ind[i] = e_now
-                    curr_ev = Event(timing, e_now, e_ind, 0, 0)
-                    det_main.is_measuring = true
+            end
+
+            # detect energy - i is main
+            for j = 1:n_det
+                if det_main.is_measuring == true
+                    if e_split[j] >= 0.1
+                        # Set trigger time, if not set. Add tof for secondary detector
+                        if curr_ev.t_trig[j] < 0
+                            curr_ev.t_trig[j] = t_now + abs(i - j) * t_tof_now
+                        end
+                        curr_ev.e_det += e_now
+                        curr_ev.n_sum += 1
+                        curr_ev.e_ind[j] += e_split[j]
+                    end
+                else
+                    if e_split[j] >= det_main.thresh
+                        # Todo: This adds trigger bias towards first detector. Should not be a problem as dt = 1e-8
+                        timing = [-1., -1.]
+                        timing[i] = t_now
+                        if t_tof_now > 0.0
+                            timing[(i % 2) + 1] = t_now + t_tof_now
+                        end
+
+                        # e_ind = 
+                        # e_ind[j] = e_split[j]
+
+                        curr_ev = Event(timing, e_now, e_split, 0, 0)
+                        det_main.is_measuring = true
+                    end
                 end
             end
         end
@@ -93,13 +132,10 @@ function run_sim(src_dict::Dict; meas_time::Float64 = 1e0, sim_step::Float64 = 1
 
     data = data[1:data_ind - 1]
 
-    println("Event rate ", length(data))
-    println("DT corr: ", dead_time_corr(length(data)))
-    tot_rate = 0.
-    for src in src_dict
-        tot_rate += src[2].rate
-    end
-    println("Theor. rate of sources", tot_rate * meas_time)
+    @printf "Obs. trigger rate %.0f \n" length(data) / (n_det * meas_time)
+    println("Event counts ", length(data))
+    @printf "DT corr. rate  %.0f \n" dead_time_corr(length(data)) / (n_det * meas_time)
+    @printf "Theor. pure rate of srcs %.0f \n" theo_rate
 
     return data
 end
