b92a2b6f455b66c86f09209aa3c8b714046f0006
Max Lamparth <72386596+maxlampe@users.noreply.github.com>
2022-10-21 20:09:29 +0200

    feat: Minor experiment changes.

--- src/main.jl
+++ src/main.jl
@@ -5,7 +5,7 @@ include("plotting.jl")
 include("tools.jl")
 
 # Bi trigger rate ca. 4500 Hz; Bg trigger rate ca 500 Hz
-bi_rate = 4.6e1
+bi_rate = 4.6e3
 gain = 1. # [1/keV] or 30.89 [ch/keV]
 srcs = Dict(
     "bi1" => Source(bi_rate * 0.7 / 7.0, gain * 450., gain * 35.),
@@ -14,13 +14,23 @@ srcs = Dict(
     "bi2" => Source(bi_rate * 4. / 7.0, gain * 1000., gain * 60.),
     "bi0/bg" => Source(bi_rate * 1.95 / 7.0, gain * 70., gain * 19.),
 )
-srcs = Dict(
-    "mono" => Source(bi_rate, gain * 400., gain * 0.),
-)
 
-res = run_sim(srcs; n_det=2, meas_time=5., with_bs=false)
+res, stat = run_sim(srcs; n_det=2, meas_time=20.0, with_bs=true)
 plot_e_hist(res, gain)
 plot_diff_both_trig(res)
 plot_e_acc_hist(res, gain)
 plot_e_both_trig(res, gain)
 plot2D_e_both_trig(res, gain)
+
+res2, stat2 = run_sim(srcs; n_det=2, meas_time=20.0, with_bs=false)
+plot_e_hist(res2, gain)
+plot_diff_both_trig(res2)
+plot_e_acc_hist(res2, gain)
+plot_e_both_trig(res2, gain)
+plot2D_e_both_trig(res2, gain)
+
+# using BenchmarkTools
+
+# res, stat = run_sim(srcs; n_det=2, meas_time=0.1, with_bs=true)
+# @btime run_sim(srcs; n_det=2, meas_time=0.1, with_bs=true)
+# @code_warntype run_sim(srcs; n_det=2, meas_time=0.1, with_bs=true)
